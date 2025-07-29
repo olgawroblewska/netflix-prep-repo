a86835e339254695fc19cdd4d8fa49f33903edc8
Olga Wroblewska <olga.wroblewska@proton.me>
2025-07-29 13:29:34 +0200

    Initial commit

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,6 +12,8 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
+  // timeout: 1000,
+
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
@@ -32,6 +34,10 @@ export default defineConfig({
     trace: 'on-first-retry',
   },
 
+  expect: {
+    timeout: 8000,
+  },
+  
   /* Configure projects for major browsers */
   projects: [
     {
